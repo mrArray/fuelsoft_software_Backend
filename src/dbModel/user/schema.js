11b02be4ac2ac414914e9eb85a73371544100de5
mrArray <nouramousser@gmail.com>
2022-07-06 18:20:37 +0100

    send array as request

--- src/dbModel/user/schema.js
+++ src/dbModel/user/schema.js
@@ -2,241 +2,324 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-  {
-    name: {
-      type: String,
-      unique: true,
+    {
+        name: {
+            type: String,
+            unique: true,
+        },
+        email: {
+            type: String,
+            require: true,
+            max: 255,
+            required: true
+
+        },
+        phoneNumber: {
+            type: String,
+            require: true,
+            max: 9
+
+        },
+        password: {
+            type: String,
+            require: true,
+
+        },
     },
-    email: {
-      type: String,
-      require: true,
-      max: 255,
-      required: true
-
-    },
-    phoneNumber: {
-      type: String,
-      require: true,
-      max: 9
-
-    },
-    password: {
-      type: String,
-      require: true,
-
-    },
-  },
 
 
 
 
-  // TODO: find out what these 2 options do
-  {
-    toJSON: {
-      virtuals: true
-    },
-    toObject: {
-      virtuals: true
-    },
-    timestamps: true
-  }
+    // TODO: find out what these 2 options do
+    {
+        toJSON: {
+            virtuals: true
+        },
+        toObject: {
+            virtuals: true
+        },
+        timestamps: true
+    }
 );
 
 const branchSchema = new Schema(
-  {
-      branch_address: {
-          type: String,
-          required: true
-      },
-      no_of_storage_tank: {
-          type: Number,
-          required: true
-      },
-      mainboard_image: {
-          type: String,
-          unique: true,
-          required: true
-      },
-      dispenser_brand: {
-          type: String,
-          unique: true,
-          required: true
-      },
-      type_of_fuel: {
-          type: String,
-          unique: true,
-          required: true
-      },
-      no_fuel_dispenser: {
-          type: Number,
-          unique: true,
-          required: true
-      },
-      tank_height: {
-          type: Number,
-          unique: true,
-          required: true
-      },
-      type_of_storage: {
-          type: String,
-          unique: true,
-          required: true
-      },
-      attached_calib_chart: {
-          type: String,
-          unique: true,
-          required: true
-      },
-      priceboard_brand: {
-          type: String,
-          unique: true,
-          required: true
-      },
+    {
+        branch_address: {
+            type: String,
+            required: true
+        },
+        no_of_storage_tank: {
+            type: Number,
+            required: true
+        },
+        mainboard_image: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        dispenser_brand: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        type_of_fuel: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        no_fuel_dispenser: {
+            type: Number,
+            unique: true,
+            required: true
+        },
+        tank_height: {
+            type: Number,
+            unique: true,
+            required: true
+        },
+        type_of_storage: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        attached_calib_chart: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        priceboard_brand: {
+            type: String,
+            unique: true,
+            required: true
+        },
 
 
 
-  },
-  {
-      // toJSON: {
-      //   virtuals: true
-      // },
-      // toObject: {
-      //   virtuals: true
-      // },
-      timestamps: true
-  }
+    },
+    {
+        // toJSON: {
+        //   virtuals: true
+        // },
+        // toObject: {
+        //   virtuals: true
+        // },
+        timestamps: true
+    }
 );
-const transactionSchema = new Schema(
-  {
-    UserId: {
-          type: Number,
-          required: true
-      },
-      Tag: {
-          type: String,
-          required: true
-      },
-      TotalAmount: {
-          type: Number,
-          required: true
-      },
-      TotalVolume: {
-          type: Number,
-          required: true
-      },
-      Amount: {
-          type: Number,
-          required: true
-      },
-      Price: {
-          type: Number,
-          required: true
-      },
-      TCVolume: {
-          type: Number,
-          required: true
-      },
-      Volume: {
-          type: Number,
-          unique: true,
-          required: true
-      },
-      Transaction: {
-          type: Number,
-          required: true
-      },
-      Nozzle: {
-          type: Number,
-          required: true
-      },
-      Pump: {
-        type: Number,
-        required: true
-    },
-    DateTime: {
-        type: String,
-        required: true
-    },
-    DateTimeStart: {
-          type: String,
-          required: true
-      },
+// const transactionSchema = new Schema({
+//     fields: [
+//         {
+//             UserId: {
+//                 type: Number,
+//                 required: true
+//             },
+//             Tag: {
+//                 type: String,
+//                 required: true
+//             },
+//             TotalAmount: {
+//                 type: Number,
+//                 required: true
+//             },
+//             TotalVolume: {
+//                 type: Number,
+//                 required: true
+//             },
+//             Amount: {
+//                 type: Number,
+//                 required: true
+//             },
+//             Price: {
+//                 type: Number,
+//                 required: true
+//             },
+//             TCVolume: {
+//                 type: Number,
+//                 required: true
+//             },
+//             Volume: {
+//                 type: Number,
+//                 unique: true,
+//                 required: true
+//             },
+//             Transaction: {
+//                 type: Number,
+//                 required: true
+//             },
+//             Nozzle: {
+//                 type: Number,
+//                 required: true
+//             },
+//             Pump: {
+//                 type: Number,
+//                 required: true
+//             },
+//             DateTime: {
+//                 type: String,
+//                 required: true
+//             },
+//             DateTimeStart: {
+//                 type: String,
+//                 required: true
+//             },
+//             Pushed: {
+//                 type: Boolean,
+//                 required: true,
+//                 default:true
+//             },
+//         }
+//     ]
+// },
 
-  },
-  {
-      // toJSON: {
-      //   virtuals: true
-      // },
-      // toObject: {
-      //   virtuals: true
-      // },
-      timestamps: true
-  }
+//     {
+//         // toJSON: {
+//         //   virtuals: true
+//         // },
+//         // toObject: {
+//         //   virtuals: true
+//         // },
+//         timestamps: true
+//     }
+// );
+
+
+const transactionSchema = new Schema({
+   
+            UserId: {
+                type: Number,
+                required: true
+            },
+            Tag: {
+                type: String,
+                required: true
+            },
+            TotalAmount: {
+                type: Number,
+                required: true
+            },
+            TotalVolume: {
+                type: Number,
+                required: true
+            },
+            Amount: {
+                type: Number,
+                required: true
+            },
+            Price: {
+                type: Number,
+                required: true
+            },
+            TCVolume: {
+                type: Number,
+                required: true
+            },
+            Volume: {
+                type: Number,
+                unique: true,
+                required: true
+            },
+            Transaction: {
+                type: Number,
+                required: true
+            },
+            Nozzle: {
+                type: Number,
+                required: true
+            },
+            Pump: {
+                type: Number,
+                required: true
+            },
+            DateTime: {
+                type: String,
+                required: true
+            },
+            DateTimeStart: {
+                type: String,
+                required: true
+            },
+            Pushed: {
+                type: Boolean,
+                required: true,
+                default:true,
+            },
+       
+},
+
+    {
+        // toJSON: {
+        //   virtuals: true
+        // },
+        // toObject: {
+        //   virtuals: true
+        // },
+        timestamps: true
+    }
 );
 
 const pumpDetailSchema = new Schema(
-  {
-      pumpDetail_address: {
-          type: String,
-          required: true
-      },
-      no_of_storage_tank: {
-          type: Number,
-          required: true
-      },
-      mainboard_image: {
-          type: String,
-          unique: true,
-          required: true
-      },
-      dispenser_brand: {
-          type: String,
-          unique: true,
-          required: true
-      },
-      type_of_fuel: {
-          type: String,
-          unique: true,
-          required: true
-      },
-      no_fuel_dispenser: {
-          type: Number,
-          unique: true,
-          required: true
-      },
-      tank_height: {
-          type: Number,
-          unique: true,
-          required: true
-      },
-      type_of_storage: {
-          type: String,
-          unique: true,
-          required: true
-      },
-      attached_calib_chart: {
-          type: String,
-          unique: true,
-          required: true
-      },
-      priceboard_brand: {
-          type: String,
-          unique: true,
-          required: true
-      },
+    {
+        pumpDetail_address: {
+            type: String,
+            required: true
+        },
+        no_of_storage_tank: {
+            type: Number,
+            required: true
+        },
+        mainboard_image: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        dispenser_brand: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        type_of_fuel: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        no_fuel_dispenser: {
+            type: Number,
+            unique: true,
+            required: true
+        },
+        tank_height: {
+            type: Number,
+            unique: true,
+            required: true
+        },
+        type_of_storage: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        attached_calib_chart: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        priceboard_brand: {
+            type: String,
+            unique: true,
+            required: true
+        },
 
 
 
-  },
-  {
-      // toJSON: {
-      //   virtuals: true
-      // },
-      // toObject: {
-      //   virtuals: true
-      // },
-      timestamps: true
-  }
+    },
+    {
+        // toJSON: {
+        //   virtuals: true
+        // },
+        // toObject: {
+        //   virtuals: true
+        // },
+        timestamps: true
+    }
 );
 
 const pumpSchema = new Schema(
@@ -253,8 +336,8 @@ const pumpSchema = new Schema(
             type: Number,
             required: true
         }
-        
-  
+
+
     },
     {
         // toJSON: {
@@ -265,12 +348,12 @@ const pumpSchema = new Schema(
         // },
         timestamps: true
     }
-  );
+);
 
-  const tagSchema = new Schema(
+const tagSchema = new Schema(
     {
 
-       
+
         Tag: {
             type: String,
             required: true
@@ -283,17 +366,17 @@ const pumpSchema = new Schema(
             type: Boolean,
             required: true
         }
-        
-  
+
+
     },
     {
-       
+
         timestamps: true
     }
-  );
-  
+);
+
 userSchema.index({
-  userId: 1
+    userId: 1
 });
 
 
--- src/service/transaction/index.js
+++ src/service/transaction/index.js
@@ -3,106 +3,119 @@
 
 //create transaction
 const createTransaction = async (fuelStationDbConnection, body) => {
-    try {
-      // const Transaction = require('../../dbModel/transaction/schema');
-  
-      const Transactions = await fuelStationDbConnection.model('Transaction');
-      
-      const UserId = body.UserId;
-      const Tag = body.Tag;
-      const TotalAmount = body.TotalAmount;
-      const TotalVolume = body.TotalVolume;
-      const Amount = body.Amount;
-      const Price = body.Price;
-      const Transaction = body.Transaction;
-      const TCVolume = body.TCVolume;
-      const Volume = body.Volume;
-      const Nozzle = body.Nozzle;
-      const Pump = body.Pump;
-      const DateTime = body.DateTime;
-      const DateTimeStart = body.DateTimeStart; 
-           
-      const transactionPresent = await Transactions.findOne({
-        DateTimeStart
-      });
+  try {
+
+    const Transactions = await fuelStationDbConnection.model('Transaction');
+    // const result=[];
+    // const obj={};
+    // console.log("body error", body);
+
+    // for (let i = 0; i < body.fields.length; i++) {
+
+    //      obj.UserId = body.fields[i].UserId;
+    //      obj.Tag = body.fields[i].Tag;
+    //      obj.TotalAmount = body.fields[i].TotalAmount;
+    //      obj.TotalVolume = body.fields[i].TotalVolume;
+    //      obj.Amount = body.fields[i].Amount;
+    //      obj.Price = body.fields[i].Price;
+    //      obj.Transaction = body.fields[i].Transaction;
+    //      obj.TCVolume = body.fields[i].TCVolume;
+    //      obj.Volume = body.fields[i].Volume;
+    //      obj.Nozzle = body.fields[i].Nozzle;
+    //      obj.Pump = body.fields[i].Pump;
+    //      obj.DateTime = body.fields[i].DateTime;
+    //      obj.DateTimeStart = body.fields[i].DateTimeStart; 
+    //      obj.Pushed = body.fields[i].true; 
+    //      result.push({ ...obj });
+    //      const DateTimeStart = body.fields[i].DateTimeStart; 
+    // }
+
+    // console.log("lets see", ...body);
+
+    for (let i = 0; i < body.length; i++) {
+
+      const DateTimeStart = body[i].DateTimeStart;
+      const transactionPresent = await Transactions.findOne(
+        {
+          DateTimeStart
+
+        }
+      );
       if (transactionPresent) {
         throw new Error("Transaction Already Exist");
       }
-      const newTransaction = await new Transactions({
-        UserId,
-        Tag,
-        TotalAmount,
-        TotalVolume,
-        Amount,
-        Price,
-        Transaction,
-        TCVolume,
-        Volume,
-        Nozzle,
-        Pump,
-        DateTime,
-        DateTimeStart
+    }
+    //  Updating properties in multiple objects
+   
+    const newTrx = body.map(obj => {
+      if (obj.Pushed === false) {
+        return { ...obj, Pushed: true };
+      }
 
-      }).save();
-      return newTransaction;
-    } catch (error) {
-      // console.log("createTransaction error", error);
-      throw error;
-    }
-  };
-  
-  //get all transactiones
-  
-  const getAllTransactiones = async fuelStationDbConnection => {
-    try {
-      const Transaction = await fuelStationDbConnection.model("Transaction");
-      const transactiones = await Transaction.find({});
-      // console.log("getAllTransactions transactiones", transactiones);
-  
-      return transactiones;
-    } catch (error) {
-      // console.log("getAllTransactions error", error);
-      throw error;
-    }
-  };
-  
-  //get transaction by id view
-  
-  const getTransactionById = async fuelStationDbConnection => {
-    try {
-      const Transaction = await fuelStationDbConnection.model("Transaction");
-      return Transaction;
-    } catch (error) {
-      // console.log("getAllTransactions error", error);
-      throw error;
-    }
-  };
-  
-  //get transaction by id delete
-  
-  const getDeleteById = async fuelStationDbConnection => {
-    try {
-      const Transaction = await fuelStationDbConnection.model("Transaction");
-      return Transaction;
-    } catch (error) {
-      // console.log("getAllTransactions error", error);
-      throw error;
-    }
-  };
-  
-  //get transaction by id update
-  
-  const getUpdateById = async fuelStationDbConnection => {
-    try {
-      const Transaction = await fuelStationDbConnection.model("Transaction");
-      return Transaction;
-    } catch (error) {
-      // console.log("getAllTransactions error", error);
-      throw error;
-    }
-  };
-  
-  
-  
-  module.exports = { getAllTransactiones, createTransaction,getTransactionById,getDeleteById,getUpdateById };
-  
+      return obj;
+    });
+
+    const newTransaction = await Transactions.insertMany(newTrx);
+
+    return newTransaction;
+
+  } catch (error) {
+    // console.log("createTransaction error", error);
+    throw error;
+  }
+};
+
+//get all transactiones
+
+const getAllTransactiones = async fuelStationDbConnection => {
+  try {
+    const Transaction = await fuelStationDbConnection.model("Transaction");
+    const transactiones = await Transaction.find({});
+    // console.log("getAllTransactions transactiones", transactiones);
+
+    return transactiones;
+  } catch (error) {
+    // console.log("getAllTransactions error", error);
+    throw error;
+  }
+};
+
+//get transaction by id view
+
+const getTransactionById = async fuelStationDbConnection => {
+  try {
+    const Transaction = await fuelStationDbConnection.model("Transaction");
+    return Transaction;
+  } catch (error) {
+    // console.log("getAllTransactions error", error);
+    throw error;
+  }
+};
+
+//get transaction by id delete
+
+const getDeleteById = async fuelStationDbConnection => {
+  try {
+    const Transaction = await fuelStationDbConnection.model("Transaction");
+    return Transaction;
+  } catch (error) {
+    // console.log("getAllTransactions error", error);
+    throw error;
+  }
+};
+
+//get transaction by id update
+
+const getUpdateById = async fuelStationDbConnection => {
+  try {
+    const Transaction = await fuelStationDbConnection.model("Transaction");
+    return Transaction;
+  } catch (error) {
+    // console.log("getAllTransactions error", error);
+    throw error;
+  }
+};
+
+
+
+module.exports = { getAllTransactiones, createTransaction, getTransactionById, getDeleteById, getUpdateById };
